620f0e360fde0c9275f71dfc8f7f96d3375b2e4d
Harras1998 <harras1998@web.de>
2025-07-02 22:20:31 +0200

    1
    1

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="de">
       <Head>
-        <link rel="icon" href="/Favicon.png" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <body>
         <Main />
