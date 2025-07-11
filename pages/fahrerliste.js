850cc1a98b6bde0f0f0fbbdb48d8a50a668c3659
Harras1998 <harras1998@web.de>
2025-07-11 07:14:34 +0200

    Update fahrerliste.js

--- pages/fahrerliste.js
+++ pages/fahrerliste.js
@@ -140,7 +140,7 @@ function PXHeader({
             <option value="name">Sortieren: Name</option>
             {/* Weitere Sortieroptionen falls nötig */}
           </select>
-          <img src="/parkxpress-logo.png" alt="PARKXPRESS" height={58} style={{ marginLeft: 18, marginRight: 10 }} />
+          <img src="/public/Logo.png" alt="ValetXpress" height={58} style={{ marginLeft: 18, marginRight: 10 }} />
         </div>
       </div>
     </div>
