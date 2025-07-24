c3addec510aa505a7bc6eeac2620d4473b447456
Harras1998 <harras1998@web.de>
2025-07-24 18:05:12 +0200

    Update AvailabilityChecker.js

--- components/AvailabilityChecker.js
+++ components/AvailabilityChecker.js
@@ -58,7 +58,7 @@ export default function AvailabilityChecker() {
         "valet_booking_init",
         JSON.stringify({
           from: from.toISOString().split("T")[0], + "T12:00:00",
-          to: to.toISOString().split("T")[0], + "T12:00:00"
+          to: to.toISOString().split("T")[0] + "T12:00:00"
         })
       );
     }
