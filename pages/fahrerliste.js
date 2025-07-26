a324080faaa38b33c88ab2aa0df1c26243389d8c
Harras1998 <harras1998@web.de>
2025-07-26 05:46:42 +0200

    Update fahrerliste.js

--- pages/fahrerliste.js
+++ pages/fahrerliste.js
@@ -383,10 +383,10 @@ export default function FahrerListe() {
                   marginRight: 30
                 }}>
                     <span style={{ fontSize: 20, color: "#444", cursor: "pointer" }} title="Bearbeiten"✏️</span>
-  <span style={{ fontSize: 20, color: "#444", cursor: "pointer" }} title="Status">✔️</span>
-  <a href={`tel:${row.telefon}`}>
-    <span style={{ fontSize: 20, color: "#444", cursor: "pointer" }} title="Anrufen">📞</span>
-  </a>
+<span style={{ fontSize: 20, color: "#444", cursor: "pointer" }} title="Status">✔️</span>
+<a href={`tel:${row.telefon}`}>
+  <span style={{ fontSize: 20, color: "#444", cursor: "pointer" }} title="Anrufen">📞</span>
+</a>
 </div>
               </div>
             ))}
