b12d3a774251dcf5eab69bebea96be8cb381d1ce
Harras1998 <harras1998@web.de>
2025-09-08 05:24:58 +0200

    Update fahrerliste.js

--- pages/fahrerliste.js
+++ pages/fahrerliste.js
@@ -1123,8 +1123,8 @@ onClick={() => {
 
 {editBuchung && (
             <div style={{
-              position: "absolute", top: 0, left: 0, right: 0, bottom: 0,
-              width: "100%", height: "100%",
+              position: "absolute", top: 0, left: 0, right: 0,
+              width: "100%", height: "100vh",
               background: "#fff", zIndex: 10000, overflowY: "auto"
             }}>
               <div style={{
